f4f17ad7fe3b11182fbebe02ca62ec74f6375e3b
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-10-31 11:15:23 +0300

    Updates

---
+++ precache-manifest.465f72685b1c26c963770edd9c848c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8233139f94bc127f566a",
+    "url": "/theater-cinemax/static/js/runtime~main.8233139f.js"
+  },
+  {
+    "revision": "7e1707f9741450dd8de7",
+    "url": "/theater-cinemax/static/js/main.7e1707f9.chunk.js"
+  },
+  {
+    "revision": "7539f7fd27172e7a97d2",
+    "url": "/theater-cinemax/static/js/1.7539f7fd.chunk.js"
+  },
+  {
+    "revision": "7e1707f9741450dd8de7",
+    "url": "/theater-cinemax/static/css/main.d04d22df.chunk.css"
+  },
+  {
+    "revision": "3173b6d183383db36e19ebffe4155e46",
+    "url": "/theater-cinemax/index.html"
+  }
+];
